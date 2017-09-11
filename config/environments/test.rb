07ef6b707fdc74dfb82097ffee4716b616c073fe
lollypop27 <lara@adafctory.co.za>
2017-09-11 10:37:45 +0200

    adds test to fix failing assets being available

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Using this fix to fake assets being available
+  Rails.application.config.assets.compile = false
+  Sprockets::Rails::Helper.prepend(Module.new do
+    def resolve_asset_path(path, *)
+      super || path
+    end
+  end)
 end
